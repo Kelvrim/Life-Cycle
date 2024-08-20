e498416d0e524b139aea5499d0ee68e6a2969e50
Adam Marx <amarx097@gmail.com>
2024-08-20 11:58:15 -0400

    Finished brute force game of life. So far it:
    - prints to screen (no graphics)
    - is unoptimized (lots of n^2 functions)
    - is relatively messy (should be refactored into classes)

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,32 @@ public class Main {
     public static void main(String[] args) {
         int width = 10;
         int height = 10;
+        int time = 5;       // How many 'turns' we play for
 
         boolean[][] grid = new boolean[width][height];
         defaultOrientation(grid);
 
+        System.out.println("First generation:");
+        printGrid(grid);
+
+        //System.out.print(countAliveNeighbors(grid, 6, 4));
+        while (time != 0){
+            nextGeneration(grid);
+            time--;
+        }
+    }
+
+    /**
+     * prints as text to system
+     * @param grid
+     */
+    private static void printGrid(boolean[][] grid) {
+        int width = grid.length;
+        int height = grid[0].length;
+
         for (int i = 0; i < width; i++){
             for (int j = 0; j < height; j++){
-                if (grid[i][j] == false){
+                if (!grid[i][j]){
                     System.out.print("` ");
                 } else {
                     System.out.print("* ");
@@ -18,6 +37,11 @@ public class Main {
         }
     }
 
+    /**
+     * sets the 'default orientation' of the grid. This is totally arbitrary and can be
+     * changed
+     * @param grid
+     */
     private static void defaultOrientation(boolean[][] grid) {
         grid[3][3] = true;
         grid[3][4] = true;
@@ -28,22 +52,66 @@ public class Main {
         grid[7][4] = true;
     }
 
-    private static void nextGeneration(boolean[][] grid, int width, int height){
+    /**
+     * Bread and butter. Iterates through the grid and updates the futureGrid
+     * based on the rules of life (in applyRulesOfLife)
+     * @param grid
+     */
+    private static void nextGeneration(boolean[][] grid){
+        int width = grid.length;
+        int height = grid[0].length;
         boolean[][] futureGrid = grid;
 
         for (int i = 0; i < width; i++){
             for (int j = 0; j < height; j++){
-                int aliveNeighbors = 0;
-                for (int i2 = -1; i2 < 2; i2++){
-                    for (int j2 = -1; j2 < 2; j2++){
+                int aliveNeighbors = countAliveNeighbors(grid, i, j);
 
-                    }
+                // Cell must be subtracted from neighbors as it was counted before
+                if (grid[i][j]){
+                    aliveNeighbors--;
                 }
+
+                futureGrid[i][j] = applyRulesOfLife(grid, i, j, aliveNeighbors);
             }
+        }
+
+        System.out.println("Next generation:");
+        printGrid(futureGrid);
+    }
+
+    private static boolean applyRulesOfLife(boolean[][] grid, int i, int j, int aliveNeighbors) {
+        // Underpopulated: cell dies
+        if (grid[i][j] && aliveNeighbors < 2){
+            return false;
+        }
+        // Overpopulated: cell dies
+        else if (grid[i][j] && aliveNeighbors > 3){
+            return false;
+        }
+        // The cell is empty and population is right: cell born
+        else if (!grid[i][j] && aliveNeighbors == 3){
+            return true;
+        }
+        else {
+            return grid[i][j];
         }
     }
 
+    private static int countAliveNeighbors(boolean[][] grid, int i, int j) {
+        int aliveNeighbors = 0;
+        for (int i2 = -1; i2 < 2; i2++){
+            for (int j2 = -1; j2 < 2; j2++){
+                if ((i + i2 >= 0 && i + i2 < grid.length) &&
+                        (j + j2 >= 0 && j + j2 < grid[0].length)){
+                    if (grid[i + i2][j + j2]){
+                         aliveNeighbors++; 
+                    }
 
+                }
+            }
+        }
+        return aliveNeighbors;
+    }
 
 
 }
