d1a3ea5bac490aa7cf6b9ce750b28990396e626a
Adam Marx <amarx097@gmail.com>
2024-09-28 15:45:50 -0400

    Added a toggle for erasing

---
+++ src/Actions/PauseAction.java
@@ -0,0 +1,16 @@
+package Actions;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+public class PauseAction extends AbstractAction {
+
+    /** PROPERTIES ******************************************************************************************/
+    public static ControlPanel controlPanel; // Reference to the Grid object
+
+    //public static ControlPanel
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        //paused = !paused;
+    }
+}
